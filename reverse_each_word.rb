0ab9b3bc3f9127f9044fdb62ca72fe92bd2a8f50
Genevieve McAllister <kj.genevieve@gmail.com>
2019-08-05 17:10:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,36 @@
 def reverse_each_word(sentence)
-  sentence_ary = sentence.split(" ")
-  sentence_ary_rev = sentence_ary.collect{ |word| word.reverse }
-  sentence_rev = sentence_ary_rev.join(" ")
-  return sentence_rev
+  sentence_ary = sentence.split
+  sentence_ary.collect do |word|
+    sentence_ary_words_rev = []
+    sentence_ary_words_rev.push(word.reverse)
+  end
+  reversed_word_sentence = sentence_ary_words_rev.join
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def reverse_each_word(sentence)
+#   sentence_ary = sentence.split(" ")
+#   sentence_ary_rev = sentence_ary.collect{ |word| word.reverse }
+#   sentence_rev = sentence_ary_rev.join(" ")
+#   return sentence_rev
+# end
